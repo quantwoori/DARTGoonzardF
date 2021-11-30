6e99adfc343af1d34f374fa95f0e0c625b5961a3
quantwoori <sangil.bae@wooriam.kr>
2021-11-30 14:15:20 +0900

    [Security] API key

--- util/Uapikey.py
+++ util/Uapikey.py
@@ -8,7 +8,7 @@ def get_token(target:str) -> str:
     :param target: type in the name for certain api
     :return: api key in str
     """
-    loc = "./security/api.json"  # refract this file to add subtract info.
+    loc = "../security/api.json"  # refract this file to add subtract info.
     with open(loc, 'r') as file:
         dat = json.load(file)
     file.close()
@@ -20,7 +20,3 @@ if __name__ == '__main__':
     # Test
     print(get_token('dart'))
     print(len(get_token('dart')))
-
-    # Test
-    print(get_token('datagov'))
-    print(len(get_token('datagov')))
